3b31b5593592e8028277cc528934cd9bb27ebee9
VinishMachado <155185076+VinshMachado@users.noreply.github.com>
2026-03-06 22:36:29 +0530

    using open relay trun

--- frontend/app/dashboard/Createmeeting/page.tsx
+++ frontend/app/dashboard/Createmeeting/page.tsx
@@ -17,10 +17,14 @@ interface MessageSchema {
 const configuration = {
   iceServers: [
     {
-      urls: ["stun:stun1.l.google.com:19302", "stun:stun2.l.google.com:19302"],
+      urls: ["stun:stun.l.google.com:19302"],
+    },
+    {
+      urls: "turn:openrelay.metered.ca:80",
+      username: "openrelayproject",
+      credential: "openrelayproject",
     },
   ],
-  iceCandidatePoolSize: 10,
 };
 
 const page = () => {
--- frontend/app/dashboard/joinMeeting/page.tsx
+++ frontend/app/dashboard/joinMeeting/page.tsx
@@ -21,7 +21,12 @@ interface MessageSchema {
 const configuration = {
   iceServers: [
     {
-      urls: ["stun:stun1.l.google.com:19302", "stun:stun2.l.google.com:19302"],
+      urls: ["stun:stun.l.google.com:19302"],
+    },
+    {
+      urls: "turn:openrelay.metered.ca:80",
+      username: "openrelayproject",
+      credential: "openrelayproject",
     },
   ],
 };
